401055154358e682b667269e65ab69ee39ab2132
nvsfs <nataliafrs@Natalias-MacBook-Pro-3.local>
2015-04-29 17:11:15 -0300

    Initial Commit

---
+++ canvas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  canvas
+//
+//  Created by Natalia Souza on 4/29/15.
+//  Copyright (c) 2015 Natalia Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ canvas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  canvas
+//
+//  Created by Natalia Souza on 4/29/15.
+//  Copyright (c) 2015 Natalia Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
